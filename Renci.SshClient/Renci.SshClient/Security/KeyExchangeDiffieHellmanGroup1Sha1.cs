2338f5aac4909ab3312faf81224b84148caf8c66
olegkap_cp <SND\olegkap_cp>
2010-12-17 13:34:00 +0000

    Add "diffie-hellman-group14-sha1" implementation

---
+++ Renci.SshClient/Renci.SshClient/Security/KeyExchangeDiffieHellmanGroup1Sha1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Security.Cryptography;
+using Renci.SshClient.Common;
+using Renci.SshClient.Messages;
+using Renci.SshClient.Messages.Transport;
+using System.Globalization;
+
+namespace Renci.SshClient.Security
+{
+    internal class KeyExchangeDiffieHellmanGroup1Sha1 : KeyExchangeDiffieHellman
+    {
+        private static object _lock = new object();
+
+        private static BigInteger _prime;
+
+        private static BigInteger _group = new BigInteger(new byte[] { 2 });
+
+        protected override BigInteger Prime
+        {
+            get 
+            {
+                if (_prime.IsZero)
+                {
+                    lock (_lock)
+                    {
+                        if (_prime.IsZero)
+                        {
+                            var secondOkleyGroup = @"00FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE65381FFFFFFFFFFFFFFFF";
+                            BigInteger.TryParse(secondOkleyGroup, System.Globalization.NumberStyles.AllowHexSpecifier, CultureInfo.CurrentCulture, out KeyExchangeDiffieHellmanGroup1Sha1._prime);
+                        }
+                    }
+                }
+            
+                return KeyExchangeDiffieHellmanGroup1Sha1._prime; 
+            }
+        }
+
+        protected override BigInteger Group
+        {
+            get { return KeyExchangeDiffieHellmanGroup1Sha1._group; }
+        }
+    }
+}

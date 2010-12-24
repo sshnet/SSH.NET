ad100df7b83b3e44f60b8076c7312cb8bcc234ac
olegkap_cp <olegkap@gmail.com>
2010-12-24 04:20:33 +0000

    Add KeyboardInteractiveConnectionInfo class for use with Keyboar interactive authentication method

---
+++ Renci.SshClient/Renci.SshClient/KeyboardInteractiveConnectionInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Renci.SshClient
+{
+    public class KeyboardInteractiveConnectionInfo : ConnectionInfo
+    {
+        public KeyboardInteractiveConnectionInfo(string host, string username)
+            : this(host, 22, username)
+        {
+
+        }
+
+        public KeyboardInteractiveConnectionInfo(string host, int port, string username)
+            : base(host, port, username)
+        {
+        }
+
+    }
+}

3156167c0d375c3a552468a6aeb75334378db8a0
Kenneth_aa_cp <SND\Kenneth_aa_cp>
2011-05-19 09:01:21 +0000

    Added test for #713. Added test Test_PortForwarding_Local_Stop_Hangs_On_Wait.

--- Renci.SshClient/Renci.SshNet.Tests/SshClientTests/TestPortForwarding.cs
+++ Renci.SshClient/Renci.SshNet.Tests/SshClientTests/TestPortForwarding.cs
@@ -84,6 +84,82 @@ namespace Renci.SshNet.Tests.SshClientTests
 			}
 		}
 
+        [TestMethod]
+        [WorkItem(713)]
+        [Owner("kenneth_aa")]
+        [Description("Test if calling Stop on ForwardedPortLocal instance causes wait.")]
+        public void Test_PortForwarding_Local_Stop_Hangs_On_Wait()
+        {
+            using (var client = new SshClient(Resources.HOST, Int32.Parse(Resources.PORT), Resources.USERNAME, Resources.PASSWORD))
+            {
+                client.Connect();
+
+                var port1 = client.AddForwardedPort<ForwardedPortLocal>("localhost", 8084, "www.google.com",80);
+                port1.Exception += delegate(object sender, ExceptionEventArgs e)
+                {
+                    Assert.Fail(e.Exception.ToString());
+                };
+
+                port1.Start();
+
+                bool hasTestedTunnel = false;
+                System.Threading.ThreadPool.QueueUserWorkItem(delegate(object state)
+                {
+                    try
+                    {
+                        var url = "http://www.google.com/";
+                        Debug.WriteLine("Starting web request to \"" + url + "\"");
+                        HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+                        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                        Assert.IsNotNull(response);
+
+                        Debug.WriteLine("Http Response status code: " + response.StatusCode.ToString());
+
+                        response.Close();
+
+                        hasTestedTunnel = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        Assert.Fail(ex.ToString());
+                    }
+
+                });
+
+                // Wait for the web request to complete.
+                while(!hasTestedTunnel)
+                {
+                    System.Threading.Thread.Sleep(1000);
+                }
+
+                try
+                {
+                    // Try stop the port forwarding, wait 3 seconds and fail if it is still started.
+                    System.Threading.ThreadPool.QueueUserWorkItem(delegate(object state)
+                    {
+                        Debug.WriteLine("Trying to stop port forward.");
+                        port1.Stop();
+                        Debug.WriteLine("Port forwarding stopped.");
+
+                    });
+
+                    System.Threading.Thread.Sleep(3000);
+                    if (port1.IsStarted)
+                    { 
+                        Assert.Fail("Port forwarding not stopped.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Assert.Fail(ex.ToString());
+                }
+                client.Disconnect();
+                Debug.WriteLine("Success.");
+            }
+        }
+
+
 		[TestMethod]
 		public void Test_PortForwarding_Remote()
 		{

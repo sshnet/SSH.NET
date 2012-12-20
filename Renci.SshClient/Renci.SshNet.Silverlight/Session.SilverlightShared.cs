e0d135b69d7a8901eddd8a443b41ee3476d1d3af
olegkap_cp <SND\olegkap_cp>
2012-12-20 05:33:31 +0000

    Code Cleanup

--- Renci.SshClient/Renci.SshNet.Silverlight/Session.SilverlightShared.cs
+++ Renci.SshClient/Renci.SshNet.Silverlight/Session.SilverlightShared.cs
@@ -1,14 +1,6 @@
 ﻿using System;
 using System.Linq;
 using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 using System.Net.Sockets;
 using System.Threading;
 using Renci.SshNet.Common;

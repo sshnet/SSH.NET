2e400ef632055b8f07ce4424c356a223425db5e4
olegkap_cp <olegkap@gmail.com>
2012-12-20 16:03:39 +0000

    Support multiple chunks in NetConf. Apply 13499 Patch

--- Renci.SshClient/Renci.SshNet.NET35/Common/Extensions.NET35.cs
+++ Renci.SshClient/Renci.SshNet.NET35/Common/Extensions.NET35.cs
@@ -66,5 +66,17 @@ namespace Renci.SshNet
 
             algorithm.Clear();
         }
+
+        /// <summary>
+        ///     Clears the contents of the string builder.
+        /// </summary>
+        /// <param name="value">
+        ///     The <see cref="StringBuilder"/> to clear.
+        /// </param>
+        public static void Clear(this StringBuilder value)
+        {
+            value.Length = 0;
+            value.Capacity = 16;
+        }
     }
 }

a0dd9837cd4184dd0913d2f2626bdc22641aba2e
olegkap_cp <SND\olegkap_cp>
2012-12-20 21:38:35 +0000

    Add Silverlight5 and WindowsPhone8 projects

---
+++ Renci.SshClient/Renci.SshNet.Silverlight5/Session.SilverlightShared.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Renci.SshNet.Common;
+using Renci.SshNet.Messages.Transport;
+using System.Text;
+using Renci.SshNet.Messages;
+
+namespace Renci.SshNet
+{
+    public partial class Session
+    {
+        private AutoResetEvent _autoEvent = new AutoResetEvent(false);
+        private AutoResetEvent _sendEvent = new AutoResetEvent(false);
+        private AutoResetEvent _receiveEvent = new AutoResetEvent(false);
+
+        private bool _isConnected = false;
+
+        partial void IsSocketConnected(ref bool isConnected)
+        {
+            isConnected = (!this._isDisconnecting && this._socket != null && this._socket.Connected && this._isAuthenticated && this._messageListenerCompleted != null && this._isConnected);
+        }
+
+        partial void SocketConnect(string host, int port)
+        {
+            var ep = new DnsEndPoint(host, port);
+            this._socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+
+            args.UserToken = this._socket;
+            args.RemoteEndPoint = ep;
+            args.Completed += new EventHandler<SocketAsyncEventArgs>(OnConnect);
+
+            this._socket.ConnectAsync(args);
+            this._autoEvent.WaitOne(this.ConnectionInfo.Timeout);
+
+            if (args.SocketError != SocketError.Success)
+                throw new SocketException((int)args.SocketError);
+        }
+
+        partial void SocketDisconnect()
+        {
+            this._socket.Close(10000);
+        }
+
+        partial void SocketReadLine(ref string response)
+        {
+            //  TODO:   Improve this function, currently will not work with server that send multiple lines as a first string
+
+            var buffer = new byte[1024];
+
+            StringBuilder result = new StringBuilder();
+
+            do
+            {
+                SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+                args.SetBuffer(buffer, 0, buffer.Length);
+                args.UserToken = this._socket;
+                args.RemoteEndPoint = this._socket.RemoteEndPoint;
+                args.Completed += new EventHandler<SocketAsyncEventArgs>(OnReceive);
+                this._socket.ReceiveAsync(args);
+
+                this._receiveEvent.WaitOne(this.ConnectionInfo.Timeout);
+
+                char lastChar = (char)buffer[0];
+                for (int i = 1; i < args.BytesTransferred; i++)
+                {
+                    char newChar = (char)buffer[i];
+                    if (lastChar == '\r' && newChar == '\n')
+                        break;
+
+                    result.Append(lastChar);
+                    lastChar = newChar;
+                }
+
+                if (args.BytesTransferred < buffer.Length)
+                    break;
+
+            } while (true);
+
+            response = result.ToString();
+        }
+
+        partial void SocketRead(int length, ref byte[] buffer)
+        {
+            var offset = 0;
+            int receivedTotal = 0;  // how many bytes is already received
+
+            do
+            {
+                SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+                args.SetBuffer(buffer, offset + receivedTotal, length - receivedTotal);
+                args.UserToken = this._socket;
+                args.RemoteEndPoint = this._socket.RemoteEndPoint;
+                args.Completed += new EventHandler<SocketAsyncEventArgs>(OnReceive);
+                this._socket.ReceiveAsync(args);
+
+                this._receiveEvent.WaitOne(this.ConnectionInfo.Timeout);
+
+                if (args.SocketError == SocketError.WouldBlock ||
+                    args.SocketError == SocketError.IOPending ||
+                    args.SocketError == SocketError.NoBufferSpaceAvailable)
+                {
+                    // socket buffer is probably empty, wait and try again
+                    Thread.Sleep(30);
+                    continue;
+                }
+                else if (args.SocketError != SocketError.Success)
+                {
+                    throw new SocketException((int)args.SocketError);
+                }
+
+                var receivedBytes = args.BytesTransferred;
+
+                if (receivedBytes > 0)
+                {
+                    receivedTotal += receivedBytes;
+                    continue;
+                }
+                else
+                {
+                    throw new SshConnectionException("An established connection was aborted by the software in your host machine.", DisconnectReason.ConnectionLost);
+                }
+            } while (receivedTotal < length);
+        }
+
+        partial void SocketWrite(byte[] data)
+        {
+            if (this._isConnected)
+            {
+                SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+                args.SetBuffer(data, 0, data.Length);
+                args.UserToken = this._socket;
+                args.RemoteEndPoint = this._socket.RemoteEndPoint;
+                args.Completed += new EventHandler<SocketAsyncEventArgs>(OnSend);
+
+                this._socket.SendAsync(args);
+            }
+            else
+                throw new SocketException((int)SocketError.NotConnected);
+
+        }
+
+        private void OnConnect(object sender, SocketAsyncEventArgs e)
+        {
+            this._autoEvent.Set();
+            this._isConnected = (e.SocketError == SocketError.Success);
+        }
+
+        private void OnSend(object sender, SocketAsyncEventArgs e)
+        {
+            this._sendEvent.Set();
+        }
+
+        private void OnReceive(object sender, SocketAsyncEventArgs e)
+        {
+            this._receiveEvent.Set();
+        }
+
+        partial void ExecuteThread(Action action)
+        {
+            ThreadPool.QueueUserWorkItem((o) => { action(); });
+        }
+
+        partial void InternalRegisterMessage(string messageName)
+        {
+            lock (this._messagesMetadata)
+            {
+                foreach (var item in from m in this._messagesMetadata where m.Name == messageName select m)
+                {
+                    item.Enabled = true;
+                    item.Activated = true;
+                }
+            }
+        }
+
+        partial void InternalUnRegisterMessage(string messageName)
+        {
+            lock (this._messagesMetadata)
+            {
+                foreach (var item in from m in this._messagesMetadata where m.Name == messageName select m)
+                {
+                    item.Enabled = false;
+                    item.Activated = false;
+                }
+            }
+        }
+
+    }
+}
---
+++ Renci.SshClient/Renci.SshNet.WindowsPhone8/Session.WP.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Renci.SshNet.Messages.Transport;
+using Renci.SshNet.Messages;
+using Renci.SshNet.Messages.Authentication;
+using Renci.SshNet.Messages.Connection;
+using System.Diagnostics;
+
+namespace Renci.SshNet
+{
+    public partial class Session
+    {
+        partial void HandleMessageCore(Message message)
+        {
+            if (message == null)
+                throw new ArgumentNullException("message");
+            else if (message is DisconnectMessage)
+                this.HandleMessage((DisconnectMessage)message);
+            else if (message is IgnoreMessage)
+                this.HandleMessage((IgnoreMessage)message);
+            else if (message is UnimplementedMessage)
+                this.HandleMessage((UnimplementedMessage)message);
+            else if (message is DebugMessage)
+                this.HandleMessage((DebugMessage)message);
+            else if (message is ServiceRequestMessage)
+                this.HandleMessage((ServiceRequestMessage)message);
+            else if (message is ServiceAcceptMessage)
+                this.HandleMessage((ServiceAcceptMessage)message);
+            else if (message is KeyExchangeInitMessage)
+                this.HandleMessage((KeyExchangeInitMessage)message);
+            else if (message is NewKeysMessage)
+                this.HandleMessage((NewKeysMessage)message);
+            else if (message is RequestMessage)
+                this.HandleMessage((RequestMessage)message);
+            else if (message is FailureMessage)
+                this.HandleMessage((FailureMessage)message);
+            else if (message is SuccessMessage)
+                this.HandleMessage((SuccessMessage)message);
+            else if (message is BannerMessage)
+                this.HandleMessage((BannerMessage)message);
+            else if (message is GlobalRequestMessage)
+                this.HandleMessage((GlobalRequestMessage)message);
+            else if (message is RequestSuccessMessage)
+                this.HandleMessage((RequestSuccessMessage)message);
+            else if (message is RequestFailureMessage)
+                this.HandleMessage((RequestFailureMessage)message);
+            else if (message is ChannelOpenMessage)
+                this.HandleMessage((ChannelOpenMessage)message);
+            else if (message is ChannelOpenConfirmationMessage)
+                this.HandleMessage((ChannelOpenConfirmationMessage)message);
+            else if (message is ChannelOpenFailureMessage)
+                this.HandleMessage((ChannelOpenFailureMessage)message);
+            else if (message is ChannelWindowAdjustMessage)
+                this.HandleMessage((ChannelWindowAdjustMessage)message);
+            else if (message is ChannelDataMessage)
+                this.HandleMessage((ChannelDataMessage)message);
+            else if (message is ChannelExtendedDataMessage)
+                this.HandleMessage((ChannelExtendedDataMessage)message);
+            else if (message is ChannelEofMessage)
+                this.HandleMessage((ChannelEofMessage)message);
+            else if (message is ChannelCloseMessage)
+                this.HandleMessage((ChannelCloseMessage)message);
+            else if (message is ChannelRequestMessage)
+                this.HandleMessage((ChannelRequestMessage)message);
+            else if (message is ChannelSuccessMessage)
+                this.HandleMessage((ChannelSuccessMessage)message);
+            else if (message is ChannelFailureMessage)
+                this.HandleMessage((ChannelFailureMessage)message);
+            else
+            {
+                Debug.WriteLine("SSH.NET WARNING: unknown message type {0} - may need to add new type to Session.WP.cs, HandleMessageCore method",
+                    message.GetType().FullName);
+
+                this.HandleMessage(message);
+            }
+        }
+    }
+}

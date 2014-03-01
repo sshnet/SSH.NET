8bea7cb388fc246a56018ef659e3269b44e83b50
Gert Driesen <gertdriesen@msn.com>
2014-03-01 10:59:08 +0000

    Skip headers (and message body if Content-Length headers is set) returned by HTTP proxy. Fail fast when proxy does not return HTTP 200. Added corresponding regression tests. Fixes the following issues: 1601, 1890 and 1905.

---
+++ Renci.SshClient/Renci.SshNet.Tests/Common/HttpRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Renci.SshNet.Tests.Common
+{
+    public class HttpRequest
+    {
+        public HttpRequest()
+        {
+            Headers = new List<string>();
+            MessageBody = new byte[0];
+        }
+
+        public string RequestLine { get; set; }
+        public IList<string> Headers { get; set; }
+        public byte[] MessageBody { get; set; }
+    }
+}

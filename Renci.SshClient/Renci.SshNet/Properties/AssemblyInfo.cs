9e7dbca99f9615aa70fd9ff9c8e310f1c6a97ddc
drieseng_cp <SND\drieseng_cp>
2014-03-02 09:24:32 +0000

    Sign the assemblies for .NET 3.5, .NET 4.0, Silverlight 4 and Silverlight 5.

--- Renci.SshClient/Renci.SshNet/Properties/AssemblyInfo.cs
+++ Renci.SshClient/Renci.SshNet/Properties/AssemblyInfo.cs
@@ -36,7 +36,7 @@ using System;
 [assembly: AssemblyFileVersion("2013.4.7")]
 [assembly: AssemblyInformationalVersion("2013.4.7")]
 [assembly: CLSCompliant(false)]
-[assembly: InternalsVisibleTo("Renci.SshNet.Tests")]
+[assembly: InternalsVisibleTo("Renci.SshNet.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f9194e1eb66b7e2575aaee115ee1d27bc100920e7150e43992d6f668f9737de8b9c7ae892b62b8a36dd1d57929ff1541665d101dc476d6e02390846efae7e5186eec409710fdb596e3f83740afef0d4443055937649bc5a773175b61c57615dac0f0fd10f52b52fedf76c17474cc567b3f7a79de95dde842509fb39aaf69c6c2")]
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
 #else

870bedfe55ea4a6ebe9f8b04e833f4e50e2638a3
Gert Driesen <gertdriesen@msn.com>
2014-03-23 19:05:57 +0000

    Move all shared assembly-level attribute to CommonAssemblyInfo.cs.

--- Renci.SshClient/Renci.SshNet/Properties/AssemblyInfo.cs
+++ Renci.SshClient/Renci.SshNet/Properties/AssemblyInfo.cs
@@ -1,44 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
-using System;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SshNet")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("Renci")]
-[assembly: AssemblyProduct("SSH.NET")]
-[assembly: AssemblyCopyright("Copyright © Renci 2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: AssemblyTitle("SSH.NET .NET 4.0")]
 [assembly: Guid("ad816c5e-6f13-4589-9f3e-59523f8b77a4")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2013.4.7")]
-[assembly: AssemblyFileVersion("2013.4.7")]
-[assembly: AssemblyInformationalVersion("2013.4.7")]
-[assembly: CLSCompliant(false)]
 [assembly: InternalsVisibleTo("Renci.SshNet.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f9194e1eb66b7e2575aaee115ee1d27bc100920e7150e43992d6f668f9737de8b9c7ae892b62b8a36dd1d57929ff1541665d101dc476d6e02390846efae7e5186eec409710fdb596e3f83740afef0d4443055937649bc5a773175b61c57615dac0f0fd10f52b52fedf76c17474cc567b3f7a79de95dde842509fb39aaf69c6c2")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif

039f7fea7b19059a7711882f96068fbd8eb4a3fe
Gert Driesen <gertdriesen@msn.com>
2014-04-06 09:08:14 +0000

    2013.4.6-beta1

--- Renci.SshClient/Renci.SshNet/Properties/CommonAssemblyInfo.cs
+++ Renci.SshClient/Renci.SshNet/Properties/CommonAssemblyInfo.cs
@@ -9,9 +9,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2013.4.7")]
-[assembly: AssemblyFileVersion("2013.4.7")]
-[assembly: AssemblyInformationalVersion("2013.4.7")]
+[assembly: AssemblyVersion("2013.4.6")]
+[assembly: AssemblyFileVersion("2013.4.6")]
+[assembly: AssemblyInformationalVersion("2013.4.6-beta1")]
 [assembly: CLSCompliant(false)]
 
 // Setting ComVisible to false makes the types in this assembly not visible 

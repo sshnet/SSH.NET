7d4dfa5ac433f76a2232ff6d8e288e9902482ee7
Gert Driesen <gertdriesen@msn.com>
2016-04-04 14:40:59 +0200

    Remove empty partial Extensions source file.

--- src/Renci.SshNet.WindowsPhone/Common/Extensions.WP.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Renci.SshNet.Common
-{
-    /// <summary>
-    /// Collection of different extension method specific for Windows Phone
-    /// </summary>
-    internal static partial class Extensions
-    {
-    }
-}

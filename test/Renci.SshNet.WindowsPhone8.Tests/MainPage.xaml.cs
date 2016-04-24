1da035203c6d913facb030d4622b755476c6c465
drieseng <gert.driesen@telenet.be>
2016-04-24 17:45:41 +0200

    Added test project for WP 8.0. Added few tests for abstraction classes.

---
+++ test/Renci.SshNet.WindowsPhone8.Tests/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using Microsoft.Phone.Controls;
+using Microsoft.VisualStudio.TestPlatform.Core;
+using Microsoft.VisualStudio.TestPlatform.TestExecutor;
+using vstest_executionengine_platformbridge;
+
+namespace Renci.SshNet.Tests
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var wrapper = new TestExecutorServiceWrapper();
+            new Thread(new ServiceMain((param0, param1) => wrapper.SendMessage((ContractName)param0, param1)).Run).Start();
+
+        }
+    }
+}
---
+++ test/Renci.SshNet.WindowsPhone8.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Renci.SshNet.WindowsPhone8.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Renci.SshNet.WindowsPhone8.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("911d635c-6e0e-46e8-9e8b-99bfb1790991")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]

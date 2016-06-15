ef80d2c175f2c797b41beeca5a54ecb6121969c4
Matthew Vukomanovic <matthew.vukomanovic@gmail.com>
2016-06-15 13:10:52 +0800

    Revert "Update the last set of items (comments) so that they reference SshNet.Session.InfiniteTimeSpan"
    This reverts commit ee774a3ce79d29b07e37a11a18409f9c9f5e92dd.

--- src/Renci.SshNet/ScpClient.cs
+++ src/Renci.SshNet/ScpClient.cs
@@ -21,8 +21,8 @@ namespace Renci.SshNet
         /// Gets or sets the operation timeout.
         /// </summary>
         /// <value>
-        /// The timeout to wait until an operation completes. The default value is <see cref="SshNet.Session.InfiniteTimeSpan"/>
-        /// , which indicates an infinite time-out period.
+        /// The timeout to wait until an operation completes. The default value is negative
+        /// one (-1) milliseconds, which indicates an infinite time-out period.
         /// </value>
         public TimeSpan OperationTimeout { get; set; }
 

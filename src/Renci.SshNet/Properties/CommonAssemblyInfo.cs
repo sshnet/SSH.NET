13cc56304ab47f487286a70a56b703afc2565064
drieseng <gert.driesen@telenet.be>
2016-08-01 21:05:02 +0200

    Prepare for stable release.

--- src/Renci.SshNet/Properties/CommonAssemblyInfo.cs
+++ src/Renci.SshNet/Properties/CommonAssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2016.0.0")]
 [assembly: AssemblyFileVersion("2016.0.0")]
-[assembly: AssemblyInformationalVersion("2016.0.0-beta3")]
+[assembly: AssemblyInformationalVersion("2016.0.0")]
 [assembly: CLSCompliant(false)]
 
 // Setting ComVisible to false makes the types in this assembly not visible 

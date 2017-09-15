f1e42ca47d231bbbb8d01a0e2253470fbec6fdfc
Artem Azaraev <artem.azaraev@gmail.com>
2017-09-15 10:17:37 +0300

    Fix #153 support of Task-based Asynchronous Pattern (TAP) over existing async API for SftpClient.  Added into a new project targeting .net 4.5.

---
+++ src/Renci.SshNet.NET45/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SSH.NET .NET 4.5")]
+[assembly: Guid("c5b64f05-d4d7-4ea9-886a-8c4b9602aa5b")]

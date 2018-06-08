c9f6af4897ca7c622b39706a3241ea27238500f1
Daniel Vukelich <d.vukelich@criteo.com>
2018-06-08 16:38:59 -0400

    Add support for openssh known_hosts files
    Can read a file in the format of openssh's known_hosts, which allows identity verification of ssh hosts that you connect to.  Hosts can be added to a KnownHostStore object either programatically, or by parsing an openssh known_hosts file.
    KnownHostStore can also be exported to text format, aloowing openssh or other compatible programs access to the hosts and their identities.  All features of the man 8 sshd secion on known_hosts are supported, except certificate authority
    validation, which is not available in this library yet.

---
+++ src/Renci.SshNet/KnownHost.cs
@@ -0,0 +1,305 @@
+﻿using Renci.SshNet.Security.Cryptography;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Renci.SshNet
+{
+    /// <summary>
+    /// Represents a host with a known public key
+    /// </summary>
+    internal class KnownHost
+    {
+        //If present at the start of a known_hosts entry, signifies that the hostname is sha1 hashed
+        private const string HashedHostSignifier = "|1|";
+
+        private const UInt16 DefaultSshPortNumber = 22;
+        private const string DefaultSshPortString = "22";
+
+        private const string KeyRevokeMarker = "@revoked";
+        private const string KeyCertAuthMarker = "@cert-authority";
+
+        private readonly bool _isHashed;
+        private readonly List<Tuple<string, Regex>> _plaintextHostPatterns;
+        private readonly byte[] _hashedHostName;
+        private readonly byte[] _hashSalt;
+        private readonly byte[] _pubKey;
+        private readonly string _keyType;
+        private readonly HostPubkeyMarker _hostMarker;
+
+        public enum HostValidationResponse
+        {
+            Matches,
+            DoesNoMatch,
+            KeyRevoked,
+            InvalidSignature,
+            ValidSignature
+        }
+
+        private enum HostPubkeyMarker
+        {
+            None,
+            Revoke,
+            CertAuthority,
+            Error
+        }
+
+        private static HostPubkeyMarker ParseSpecialOperand(string toParse)
+        {
+            switch (toParse)
+            {
+                case KeyRevokeMarker:
+                    return HostPubkeyMarker.Revoke;
+                case KeyCertAuthMarker:
+                    return HostPubkeyMarker.CertAuthority;
+                default:
+                    return HostPubkeyMarker.Error;
+            }
+        }
+
+        private KnownHost(string hostName, string keyType, string base64Pubkey, HostPubkeyMarker marker)
+        {
+            _isHashed = hostName.StartsWith("|");
+
+            if (_isHashed)
+            {
+                if (!hostName.StartsWith(HashedHostSignifier))
+                    throw new FormatException("The hashed section was not properly composed");
+
+                string[] splitHashedSection = hostName.Split('|');
+                if (splitHashedSection.Length != 4)
+                    throw new FormatException("The hashed section was not properly composed");
+
+                _hashSalt = Convert.FromBase64String(splitHashedSection[2]);
+
+                if (_hashSalt.Length != 20)
+                    throw new ArgumentException("The salt must be exacly 20 bytes");
+
+                _hashedHostName = Convert.FromBase64String(splitHashedSection[3]);
+            }
+            else
+            {
+                _plaintextHostPatterns = GetPatternList(hostName);
+                if (_plaintextHostPatterns.Count == 0)
+                    throw new FormatException("No hostname patterns given");
+            }
+
+            _keyType = keyType;
+            _pubKey = Convert.FromBase64String(base64Pubkey);
+            _hostMarker = marker;
+        }
+
+        /// <summary>
+        /// Attempts to parse a line from an openssh known_hosts file
+        /// </summary>
+        /// <param name="hostLine"></param>
+        /// <param name="host"></param>
+        /// <returns><c>true</c> if the host line could be parsed.  <c>false</c> if an error occurred</returns>
+        public static bool TryParse(string hostLine, out KnownHost host)
+        {
+            try
+            {
+                return UnsafeTryParse(hostLine, out host);
+            }
+            catch (FormatException)
+            {
+                host = null;
+                return false;
+            }
+            catch (ArgumentException)
+            {
+                host = null;
+                return false;
+            }
+        }
+
+        private static bool UnsafeTryParse(string hostLine, out KnownHost host)
+        {
+            host = null;
+            if (string.IsNullOrEmpty(hostLine) || hostLine.StartsWith("#"))
+                return false;
+
+            bool hasSpecialPrefix = hostLine.StartsWith("@");
+
+            string[] sectionedLine = hostLine.Split(' ');
+            if (hasSpecialPrefix && sectionedLine.Length < 4)
+                return false;
+            else if (sectionedLine.Length < 3)
+                return false;
+
+            HostPubkeyMarker marker =
+                hasSpecialPrefix ? ParseSpecialOperand(sectionedLine[0]) : HostPubkeyMarker.None;
+            if (marker == HostPubkeyMarker.Error)
+                return false;
+
+            int sectionOffset = hasSpecialPrefix ? 1 : 0;
+            string hostNameSecion = sectionedLine[sectionOffset];
+            string keyTypeSection = sectionedLine[1 + sectionOffset];
+            string pubKeySection = sectionedLine[2 + sectionOffset];
+
+            host = new KnownHost(hostNameSecion, keyTypeSection, pubKeySection, marker);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Compare the given host information with this KnownHost
+        /// </summary>
+        /// <param name="hostname"></param>
+        /// <param name="keyType"></param>
+        /// <param name="pubKey"></param>
+        /// <param name="portNumber"></param>
+        /// <returns>Whether or not the given hostname-keytype-pubkey combination is a match to this KnownHost</returns>
+        public HostValidationResponse MatchesPubKey(string hostname, string keyType, byte[] pubKey, UInt16 portNumber)
+        {
+            if (!ValidateHostName(hostname, portNumber))
+                return HostValidationResponse.DoesNoMatch;
+
+            if (_hostMarker == HostPubkeyMarker.CertAuthority)
+                return ValidateKeySignature(hostname, keyType, pubKey);
+
+            if (_keyType != keyType || !_pubKey.SequenceEqual(pubKey))
+                return HostValidationResponse.DoesNoMatch;
+
+            switch (_hostMarker)
+            {
+                case HostPubkeyMarker.Revoke:
+                    return HostValidationResponse.KeyRevoked;
+                default:
+                    return HostValidationResponse.Matches;
+            }
+        }
+
+        private HostValidationResponse ValidateKeySignature(string hostname, string keyType, byte[] keyToCheck)
+        {
+            //TODO: Return HostValidationResponse.ValidSignature if the key is signed by this Certificate Authority
+            return HostValidationResponse.InvalidSignature;
+        }
+
+        /// <summary>
+        /// Writes this KnownHost as an openssh known_hosts formatted line
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            string hostnameSection = _isHashed
+                ? string.Format("{0}{1}|{2}", HashedHostSignifier, Convert.ToBase64String(_hashSalt), Convert.ToBase64String(_hashedHostName))
+                : string.Join("," , _plaintextHostPatterns.Select(x => x.Item1));
+
+            string specialPrefix = string.Empty;
+            switch (_hostMarker)
+            {
+                case HostPubkeyMarker.CertAuthority:
+                    specialPrefix = KeyCertAuthMarker + " ";
+                    break;
+                case HostPubkeyMarker.Revoke:
+                    specialPrefix = KeyRevokeMarker + " ";
+                    break;
+            }
+
+            return string.Format("{0}{1} {2} {3}",specialPrefix, hostnameSection, _keyType, Convert.ToBase64String(_pubKey));
+        }
+
+        private bool ValidateHostName(string host, UInt16 port)
+        {
+            if (_isHashed)
+            {
+                //Corner case with hashed hosts:  If the default port is specified, then the hash could either be just the hostname,
+                //or hostname with port
+                if (port == DefaultSshPortNumber && ValidateHashedHostName(host))
+                    return true;
+                return ValidateHashedHostName(string.Format("[{0}:{1}]", host, port));
+            }
+            return ValidatePlaintextHostName(string.Format("[{0}:{1}]", host, port));
+        }
+
+        private bool ValidateHashedHostName(string hostAndPort)
+        {
+            HMACSHA1 hmac = new HMACSHA1(_hashSalt);
+            byte[] hashToCompare = hmac.ComputeHash(Encoding.ASCII.GetBytes(hostAndPort));
+
+            return _hashedHostName.SequenceEqual(hashToCompare);
+        }
+
+        private bool ValidatePlaintextHostName(string hostAndPort)
+        {
+            bool foundAtLeastOneMatch = false;
+            foreach (Tuple<string, Regex> possibleMatch in _plaintextHostPatterns)
+            {
+                bool negateMatch = possibleMatch.Item1.StartsWith("!");
+                bool foundMatch = possibleMatch.Item2.IsMatch(hostAndPort);
+                if (foundMatch && negateMatch)
+                    return false;
+
+                foundAtLeastOneMatch = foundAtLeastOneMatch || foundMatch;
+            }
+
+            return foundAtLeastOneMatch;
+        }
+
+        private List<Tuple<string, Regex>> GetPatternList(string unsplitHostPatterns)
+        {
+            List<Tuple<string, Regex>> patternList = new List<Tuple<string, Regex>>();
+            foreach (string s in unsplitHostPatterns.Split(','))
+            {
+                Regex toAdd;
+                if(!TryGetRegexFromPlaintextHostPattern(s, out toAdd))
+                    continue;
+                patternList.Add(new Tuple<string, Regex>(s, toAdd));
+            }
+
+            return patternList;
+        }
+
+        private static bool TryGetRegexFromPlaintextHostPattern(string pattern, out Regex matchingExpression)
+        {
+            matchingExpression = null;
+
+            StringBuilder regexBuilder = new StringBuilder();
+
+            string strippedHostPattern;
+            if(!TryValidateAndFormatHostPattern(pattern, out strippedHostPattern))
+                return false;
+
+            //split by *, regex escape the split sections, replace * with regex equivalent (.*)
+            string[] sectionedPattern = strippedHostPattern.Split('*');
+            regexBuilder.Append(string.Join(".*", sectionedPattern.Select(Regex.Escape)));
+
+
+            matchingExpression = new Regex(regexBuilder.ToString());
+            return true;
+        }
+
+        private static bool TryValidateAndFormatHostPattern(string input, out string hostPattern)
+        {
+            hostPattern = null;
+
+            bool squareBraceOpens = input.StartsWith("[");
+            bool squareBraceCloses = input.EndsWith("]");
+
+            //Opening/Closing braces must be matched
+            if (squareBraceOpens ^ squareBraceCloses)
+                return false;
+
+            if(squareBraceOpens)
+            {
+                //Patterns surrounded by braces must specify port
+                if (!input.Contains(":"))
+                    return false;
+                hostPattern = input;
+            }
+            else
+            {
+                hostPattern = string.Format("[{0}:{1}]", input, DefaultSshPortString);
+            }
+
+            //Strip the negation operand if present
+            if (hostPattern.StartsWith("[!"))
+                hostPattern = hostPattern.Remove(1, 1);
+
+            return true;
+        }
+    }
+}
---
+++ src/Renci.SshNet/KnownHostStore.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using Renci.SshNet.Abstractions;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Renci.SshNet.Common;
+using Renci.SshNet.Security.Cryptography;
+
+namespace Renci.SshNet
+{
+    /// <summary>
+    /// A class to save and store the public keys of connected hosts.  Capable
+    /// of reading and creating files compatible with openssh's known_hosts
+    /// format.  For more information on known_hosts files, read the
+    /// <c>man 8 sshd</c> section on known_hosts
+    /// </summary>
+    public class KnownHostStore
+    {
+        private readonly List<KnownHost> _knownHosts;
+
+        /// <summary>
+        /// Construct an empty KnownHostStore
+        /// </summary>
+        public KnownHostStore()
+        {
+            _knownHosts = new List<KnownHost>();
+        }
+
+        /// <summary>
+        /// Construct a KnownHostStore from an openssh known_hosts file
+        /// </summary>
+        /// <param name="knownHostsFile">The path of the file to read</param>
+        public KnownHostStore(string knownHostsFile)
+        {
+            _knownHosts = new List<KnownHost>();
+            ImportKnownHostsFromFile(knownHostsFile);
+        }
+
+        /// <summary>
+        /// Fill this store with elements from an openssh known_hosts file
+        /// </summary>
+        /// <param name="filePath">The path of the file to read</param>
+        public void ImportKnownHostsFromFile(string filePath)
+        {
+            using (var fStream = File.OpenText(filePath))
+            {
+                while (!fStream.EndOfStream)
+                {
+                    KnownHost createdHost;
+                    if (KnownHost.TryParse(fStream.ReadLine(), out createdHost))
+                        _knownHosts.Add(createdHost);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Add a host to this store.
+        /// </summary>
+        /// <param name="hostname">The name of the host to add</param>
+        /// <param name="portNumber"></param>
+        /// <param name="keyType">The algorithm of the public key</param>
+        /// <param name="pubKey">The public key of the host to add</param>
+        /// <param name="storeHostnameHashed">Whether the hostname should be stored as a SHA1 hash</param>
+        /// <param name="marker">An optional <c>@</c> prefixed string that corresponds to a marker for this host.  See the <c>man 8 sshd</c> section about known_hosts for valid markers</param>
+        /// <exception cref="FormatException">Thrown if the given arguments cannor be parsed into a valid host entry</exception>
+        public void AddHost(string hostname, UInt16 portNumber, string keyType, byte[] pubKey, bool storeHostnameHashed, string marker = "")
+        {
+            string hostNameWithPort = string.Format("[{0}:{1}]", hostname, portNumber);
+            string hostnameSection;
+
+            if (!string.IsNullOrEmpty(marker))
+            {
+                if(!marker.StartsWith("@"))
+                    throw new FormatException("The given host marker must be prefixed with \'@\'.  See the \'man 8 sshd\' section about known_hosts for more details on markers");
+                marker = string.Format("{0} ", marker);
+            }
+
+            if (storeHostnameHashed)
+            {
+                byte[] salt = new byte[20];
+                CryptoAbstraction.GenerateRandom(salt);
+
+                HMACSHA1 hmac = new HMACSHA1(salt);
+                byte[] hash = hmac.ComputeHash(Encoding.ASCII.GetBytes(hostNameWithPort));
+
+                hostnameSection = string.Format("|1|{0}|{1}", Convert.ToBase64String(salt), Convert.ToBase64String(hash));
+            }
+            else
+            {
+                hostnameSection = hostNameWithPort;
+            }
+
+            string hostToParse = string.Format("{3}{0} {1} {2}", hostnameSection, keyType, Convert.ToBase64String(pubKey), marker);
+
+            KnownHost newHost;
+            if(!KnownHost.TryParse(hostToParse, out newHost))
+                throw new FormatException("Malformed input: Failed to create entry.  If you specified a marker, ensure it is valid (see the \'man 8 sshd\' section on known_hosts for more details on markers)");
+
+            _knownHosts.Add(newHost);
+        }
+
+        /// <summary>
+        /// Removes all hosts from this store
+        /// </summary>
+        public void ClearHosts()
+        {
+            _knownHosts.Clear();
+        }
+
+        /// <summary>
+        /// Writes all hosts in this store to an openssh known_hosts formatted file
+        /// </summary>
+        /// <param name="outFile">The path of the file to be written</param>
+        public void ExportKnownHostsToFile(string outFile)
+        {
+            using (var fStream = File.OpenWrite(outFile))
+            {
+                foreach (KnownHost host in _knownHosts)
+                {
+                    byte[] hostLine = Encoding.ASCII.GetBytes(host.ToString() + Environment.NewLine);
+                    fStream.Write(hostLine, 0, hostLine.Length);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Look for a host in this store with the given hostname and public key
+        /// </summary>
+        /// <param name="hostname">The name of the host to search for</param>
+        /// <param name="keyType">The algorithm of the public key</param>
+        /// <param name="pubKey">The public key of the host to search for</param>
+        /// <param name="port">The port number</param>
+        /// <exception cref="RevokedKeyException">If the given key is marked as revoked</exception>
+        /// <returns>Whether a host corresponding to the given parameters exists in this store</returns>
+        public bool Knows(string hostname, string keyType, byte[] pubKey, UInt16 port)
+        {
+            bool foundMatch = false;
+
+            foreach (KnownHost host in _knownHosts)
+            {
+                KnownHost.HostValidationResponse typeOfMatch = host.MatchesPubKey(hostname, keyType, pubKey, port);
+
+                if(typeOfMatch == KnownHost.HostValidationResponse.KeyRevoked)
+                    throw new RevokedKeyException("The given host-pubkey pair is marked as revoked");
+
+                foundMatch = foundMatch || (typeOfMatch == KnownHost.HostValidationResponse.Matches);
+            }
+
+            return foundMatch;
+        }
+    }
+}

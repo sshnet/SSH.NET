c6490349593873ec26166b581e3963f4e1c8de52
smazov <smazov@ozon.ru>
2019-07-25 11:34:41 +0300

    Revert "Revert "Revert "Prioritizing of encryption algorithms (might be SECURITY HOLE)"""
    This reverts commit 46ce8a7790c3864d854a3d6413de3c542a62dae1.

--- src/Renci.SshNet/Security/KeyExchangeDiffieHellman.cs
+++ src/Renci.SshNet/Security/KeyExchangeDiffieHellman.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Text;
 using Renci.SshNet.Messages.Transport;
 using Renci.SshNet.Common;
@@ -76,8 +75,7 @@ namespace Renci.SshNet.Security
 
             var length = Pack.BigEndianToUInt32(_hostKey);
             var algorithmName = Encoding.UTF8.GetString(_hostKey, 4, (int)length);
-            var key = Session.ConnectionInfo.HostKeyAlgorithms
-                .First(x => x.Key.Value == algorithmName).Value.Invoke(_hostKey);
+            var key = Session.ConnectionInfo.HostKeyAlgorithms[algorithmName](_hostKey);
 
             Session.ConnectionInfo.CurrentHostKeyAlgorithm = algorithmName;
 

19708c9daa8e086a1c2e36c715444dd0facfdb26
Gert Driesen <gert.driesen@telenet.be>
2020-02-29 17:23:51 +0100

    Fix CLS compliance warnings.

--- src/Renci.SshNet/Security/BouncyCastle/util/Integers.cs
+++ src/Renci.SshNet/Security/BouncyCastle/util/Integers.cs
@@ -9,7 +9,6 @@ namespace Renci.SshNet.Security.Org.BouncyCastle.Utilities
             return (i << distance) ^ (int)((uint)i >> -distance);
         }
 
-        [CLSCompliantAttribute(false)]
         public static uint RotateLeft(uint i, int distance)
         {
             return (i << distance) ^ (i >> -distance);
@@ -20,7 +19,6 @@ namespace Renci.SshNet.Security.Org.BouncyCastle.Utilities
             return (int)((uint)i >> distance) ^ (i << -distance);
         }
 
-        [CLSCompliantAttribute(false)]
         public static uint RotateRight(uint i, int distance)
         {
             return (i >> distance) ^ (i << -distance);

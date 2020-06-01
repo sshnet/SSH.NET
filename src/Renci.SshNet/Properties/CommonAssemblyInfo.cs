66cf838d264b1a03c05bedbdbbf72fe6654f4308
drieseng <gert.driesen@telenet.be>
2020-06-01 10:03:51 +0200

    Prepare for 2020.0.0-beta1

--- src/Renci.SshNet/Properties/CommonAssemblyInfo.cs
+++ src/Renci.SshNet/Properties/CommonAssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("SSH.NET is a Secure Shell (SSH) library for .NET, optimized for parallelism.")]
 [assembly: AssemblyCompany("Renci")]
 [assembly: AssemblyProduct("SSH.NET")]
-[assembly: AssemblyCopyright("Copyright © Renci 2010-2017")]
+[assembly: AssemblyCopyright("Copyright © Renci 2010-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2016.1.0")]
-[assembly: AssemblyFileVersion("2016.1.0")]
-[assembly: AssemblyInformationalVersion("2016.1.0")]
+[assembly: AssemblyVersion("2020.0.0")]
+[assembly: AssemblyFileVersion("2020.0.0")]
+[assembly: AssemblyInformationalVersion("2020.0.0-beta1")]
 [assembly: CLSCompliant(false)]
 
 // Setting ComVisible to false makes the types in this assembly not visible 

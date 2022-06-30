6bc23f9ec6f9faeb115bef1ae7fc7004eb0ce546
Owen Krueger <github@owenkrueger.dev>
2022-06-30 11:04:24 -0500

    Check FEATURE_TAP

--- src/Renci.SshNet/IBaseClient.cs
+++ src/Renci.SshNet/IBaseClient.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Net.Sockets;
 using System.Threading;
+#if FEATURE_TAP
 using System.Threading.Tasks;
+#endif
 
 namespace Renci.SshNet
 {
@@ -66,6 +68,7 @@ namespace Renci.SshNet
         /// <exception cref="ProxyException">Failed to establish proxy connection.</exception>
         void Connect();
 
+#if FEATURE_TAP
         /// <summary>
         /// Asynchronously connects client to the server.
         /// </summary>
@@ -79,6 +82,7 @@ namespace Renci.SshNet
         /// <exception cref="SshAuthenticationException">Authentication of SSH session failed.</exception>
         /// <exception cref="ProxyException">Failed to establish proxy connection.</exception>
         Task ConnectAsync(CancellationToken cancellationToken);
+#endif
 
         /// <summary>
         /// Disconnects client from the server.

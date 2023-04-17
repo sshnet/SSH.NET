8a5655fc6325a3c5ca9b537c9a725bbab2c9ca34
Wojciech Nagórski <wojtpl2@gmail.com>
2023-04-17 11:58:55 +0200

    Try

--- src/Renci.SshNet.Tests/Classes/SessionTest_ConnectedBase.cs
+++ src/Renci.SshNet.Tests/Classes/SessionTest_ConnectedBase.cs
@@ -56,16 +56,16 @@ namespace Renci.SshNet.Tests.Classes
         [TestCleanup]
         public void TearDown()
         {
-            if (ServerSocket != null)
-            {
-                ServerSocket.Dispose();
-                ServerSocket = null;
-            }
-
             if (ServerListener != null)
             {
                 ServerListener.Dispose();
                 ServerListener = null;
+            }
+
+            if (ServerSocket != null)
+            {
+                ServerSocket.Dispose();
+                ServerSocket = null;
             }
 
             if (Session != null)

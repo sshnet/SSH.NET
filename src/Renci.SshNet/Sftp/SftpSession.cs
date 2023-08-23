de4b7770538314ea063e576194ba8143e33bc187
Fernando Bedoya Ortecho <t-fernandob@microsoft.com>
2023-08-23 16:16:06 -0700

    Add getRealPath parameter to GetCanonized function

--- src/Renci.SshNet/Sftp/SftpSession.cs
+++ src/Renci.SshNet/Sftp/SftpSession.cs
@@ -89,12 +89,19 @@ namespace Renci.SshNet.Sftp
         /// Resolves a given path into an absolute path on the server.
         /// </summary>
         /// <param name="path">The path to resolve.</param>
+        /// <param name="getRealPath">Boolean determining whether to get the real path.</param>
         /// <returns>
         /// The absolute path.
         /// </returns>
-        public string GetCanonicalPath(string path)
+        public string GetCanonicalPath(string path, bool getRealPath = true)
         {
             var fullPath = GetFullRemotePath(path);
+
+            if (!getRealPath)
+            {
+                // getRealPath set to false allows us to get a reference to the symbolic link itself and not to the file it points to.
+                return fullPath;
+            }
 
             var canonizedPath = string.Empty;
 

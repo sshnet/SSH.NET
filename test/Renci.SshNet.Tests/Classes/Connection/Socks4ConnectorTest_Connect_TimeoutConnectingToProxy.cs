309ae1bfccfce8fe39af7d327a6a312d0cbde9f0
Wojciech Nagórski <wojciech.nagorski@intel.com>
2023-11-25 15:04:54 +0100

    Try to fix tests

--- test/Renci.SshNet.Tests/Classes/Connection/Socks4ConnectorTest_Connect_TimeoutConnectingToProxy.cs
+++ test/Renci.SshNet.Tests/Classes/Connection/Socks4ConnectorTest_Connect_TimeoutConnectingToProxy.cs
@@ -23,7 +23,7 @@ namespace Renci.SshNet.Tests.Classes.Connection
             var random = new Random();
 
             _connectionInfo = CreateConnectionInfo("proxyUser", "proxyPwd");
-            _connectionInfo.Timeout = TimeSpan.FromMilliseconds(random.Next(50, 200));
+            _connectionInfo.Timeout = TimeSpan.FromMilliseconds(random.Next(5, 10));
             _stopWatch = new Stopwatch();
             _clientSocket = SocketFactory.Create(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             _actualException = null;

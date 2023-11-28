554c2d6ad10d9a91d50647aa201967e21ed5ac78
Pedro Fonseca <pfonseca@qti.qualcomm.com>
2023-11-28 12:06:50 +0100

    Move AesCipherMode enum to its own file

--- src/Renci.SshNet/Security/Cryptography/Ciphers/AesCipher.cs
+++ src/Renci.SshNet/Security/Cryptography/Ciphers/AesCipher.cs
@@ -7,30 +7,6 @@ using Renci.SshNet.Security.Cryptography.Ciphers.Paddings;
 namespace Renci.SshNet.Security.Cryptography.Ciphers
 {
     /// <summary>
-    /// Custom AES Cipher Mode, follows System.Security.Cryptography.CipherMode.
-    /// </summary>
-    public enum AesCipherMode
-    {
-        /// <summary>CBC Mode.</summary>
-        CBC = 1,
-
-        /// <summary>ECB Mode.</summary>
-        ECB = 2,
-
-        /// <summary>OFB Mode.</summary>
-        OFB = 3,
-
-        /// <summary>CFB Mode.</summary>
-        CFB = 4,
-
-        /// <summary>CTS Mode.</summary>
-        CTS = 5,
-
-        /// <summary>CTR Mode.</summary>
-        CTR = 6
-    }
-
-    /// <summary>
     /// AES cipher implementation.
     /// </summary>
     public sealed partial class AesCipher : BlockCipher, IDisposable

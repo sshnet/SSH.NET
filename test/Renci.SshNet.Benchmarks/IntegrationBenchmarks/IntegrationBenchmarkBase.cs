aec4168496e16bff5c17aa37b90f427b0c99f96a
Wojciech Nagórski <wojciech.nagorski@intel.com>
2023-12-06 20:59:35 +0100

    Integration benchmark tests

---
+++ test/Renci.SshNet.Benchmarks/IntegrationBenchmarks/IntegrationBenchmarkBase.cs
@@ -0,0 +1,18 @@
+﻿using Renci.SshNet.IntegrationTests.TestsFixtures;
+
+namespace Renci.SshNet.Benchmarks.IntegrationBenchmarks
+{
+    public class IntegrationBenchmarkBase
+    {
+#pragma warning disable CA1822 // Mark members as static
+        public async Task GlobalSetup()
+        {
+            await InfrastructureFixture.Instance.InitializeAsync().ConfigureAwait(false);
+        }
+        public async Task GlobalCleanup()
+        {
+            await InfrastructureFixture.Instance.DisposeAsync().ConfigureAwait(false);
+        }
+#pragma warning restore CA1822 // Mark members as static
+    }
+}
---
+++ test/Renci.SshNet.Benchmarks/IntegrationBenchmarks/Test.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+
+using Renci.SshNet.IntegrationTests.TestsFixtures;
+
+namespace Renci.SshNet.Benchmarks.IntegrationBenchmarks
+{
+    [MemoryDiagnoser]
+    [SimpleJob]
+    public class Test : IntegrationBenchmarkBase
+    {
+        private readonly InfrastructureFixture _infrastructureFixture;
+
+        [GlobalSetup]
+        public async Task Setup()
+        {
+            await GlobalSetup().ConfigureAwait(false);
+        }
+
+        [GlobalCleanup]
+        public async Task Cleanup()
+        {
+            await GlobalCleanup().ConfigureAwait(false);
+        }
+
+        public Test()
+        {
+            _infrastructureFixture = InfrastructureFixture.Instance;
+        }
+
+        [Benchmark]
+        public string Connect()
+        {
+            using var sshClient = new SshClient(_infrastructureFixture.SshServerHostName, _infrastructureFixture.SshServerPort, _infrastructureFixture.User.UserName, _infrastructureFixture.User.Password);
+            sshClient.Connect();
+            return sshClient.RunCommand("echo $'test !@#$%^&*()_+{}:,./<>[];\\|'").Result;
+        }
+    }
+}

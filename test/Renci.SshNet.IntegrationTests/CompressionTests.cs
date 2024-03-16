e7fcad3fe665c4b387afc0a469252768108f073e
Scott Xu <scott-xu@msn.com>
2024-03-16 20:36:49 +0800

    Test the compression by upload/download file

--- test/Renci.SshNet.IntegrationTests/CompressionTests.cs
+++ test/Renci.SshNet.IntegrationTests/CompressionTests.cs
@@ -29,13 +29,28 @@ namespace Renci.SshNet.IntegrationTests
 
         private void DoTest(KeyValuePair<string, Func<Compressor>> compressor)
         {
-            using (var client = new SshClient(_connectionInfoFactory.Create()))
+            using (var scpClient = new ScpClient(_connectionInfoFactory.Create()))
             {
-                client.ConnectionInfo.CompressionAlgorithms.Clear();
-                client.ConnectionInfo.CompressionAlgorithms.Add(compressor);
+                scpClient.ConnectionInfo.CompressionAlgorithms.Clear();
+                scpClient.ConnectionInfo.CompressionAlgorithms.Add(compressor);
 
-                client.Connect();
-                client.Disconnect();
+                scpClient.Connect();
+
+                Assert.AreEqual(compressor.Key, scpClient.ConnectionInfo.CurrentClientCompressionAlgorithm);
+                Assert.AreEqual(compressor.Key, scpClient.ConnectionInfo.CurrentServerCompressionAlgorithm);
+
+                var file = $"/tmp/{Guid.NewGuid()}.txt";
+                var fileContent = "RepeatingRepeatingRepeatingRepeatingRepeatingRepeatingRepeatingRepeatingRepeatingRepeatingRepeatingRepeatingRepeatingRepeatingRepeatingRepeatingRepeatingRepeatingRepeatingRepeating";
+
+                using var uploadStream = new MemoryStream(Encoding.UTF8.GetBytes(fileContent));
+                scpClient.Upload(uploadStream, file);
+
+                using var downloadStream = new MemoryStream();
+                scpClient.Download(file, downloadStream);
+
+                var result = Encoding.UTF8.GetString(downloadStream.ToArray());
+
+                Assert.AreEqual(fileContent, result);
             }
         }
     }

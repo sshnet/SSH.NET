ebb31bb16e1e95c446ae9652121c4f1b1b3761b9
Scott Xu <scott-xu@msn.com>
2024-08-19 20:13:10 +0800

    AesGcmCipher uses BouncyCastle as a fallback if BCL does not support. (#1450)
    * [AesGcmCipher] Use BouncyCastle as a fallback if BCL does not support.
    
    * Switch back to collection initializer
    
    * Remove conditional compilation
    
    * Throw SshConnectionException with Reason MacError when authentication tag mismatch
    
    * Separate BCL and BouncyCastle implementation
    
    * Update AesGcmCipher.BclImpl.cs
    
    * Naming enhancement
    
    * Remove empty line
    
    * Disable S1199. See https://github.com/sshnet/SSH.NET/pull/1371#discussion_r1704293356
    
    * Set InnerException when MAC error. Remove Message check.
    
    * Store KeyParameter as private field
    
    * Use GcmCipher.ProcessAadBytes to avoid the copy of associated data
    
    * Move nonce to constructor to avoid creating AeadParameters each packet
    
    * Use const int for tag size
    
    ---------
    
    Co-authored-by: Wojciech Nagórski <wojtpl2@gmail.com>

--- src/Renci.SshNet/ConnectionInfo.cs
+++ src/Renci.SshNet/ConnectionInfo.cs
@@ -381,22 +381,19 @@ namespace Renci.SshNet
                     { "diffie-hellman-group1-sha1", () => new KeyExchangeDiffieHellmanGroup1Sha1() },
                 };
 
-            Encryptions = new Dictionary<string, CipherInfo>();
-            Encryptions.Add("aes128-ctr", new CipherInfo(128, (key, iv) => new AesCipher(key, iv, AesCipherMode.CTR, pkcs7Padding: false)));
-            Encryptions.Add("aes192-ctr", new CipherInfo(192, (key, iv) => new AesCipher(key, iv, AesCipherMode.CTR, pkcs7Padding: false)));
-            Encryptions.Add("aes256-ctr", new CipherInfo(256, (key, iv) => new AesCipher(key, iv, AesCipherMode.CTR, pkcs7Padding: false)));
-#if NET6_0_OR_GREATER
-            if (AesGcm.IsSupported)
-            {
-                Encryptions.Add("aes128-gcm@openssh.com", new CipherInfo(128, (key, iv) => new AesGcmCipher(key, iv), isAead: true));
-                Encryptions.Add("aes256-gcm@openssh.com", new CipherInfo(256, (key, iv) => new AesGcmCipher(key, iv), isAead: true));
-            }
-#endif
-            Encryptions.Add("chacha20-poly1305@openssh.com", new CipherInfo(512, (key, iv) => new ChaCha20Poly1305Cipher(key), isAead: true));
-            Encryptions.Add("aes128-cbc", new CipherInfo(128, (key, iv) => new AesCipher(key, iv, AesCipherMode.CBC, pkcs7Padding: false)));
-            Encryptions.Add("aes192-cbc", new CipherInfo(192, (key, iv) => new AesCipher(key, iv, AesCipherMode.CBC, pkcs7Padding: false)));
-            Encryptions.Add("aes256-cbc", new CipherInfo(256, (key, iv) => new AesCipher(key, iv, AesCipherMode.CBC, pkcs7Padding: false)));
-            Encryptions.Add("3des-cbc", new CipherInfo(192, (key, iv) => new TripleDesCipher(key, new CbcCipherMode(iv), padding: null)));
+            Encryptions = new Dictionary<string, CipherInfo>
+                {
+                    { "aes128-ctr", new CipherInfo(128, (key, iv) => new AesCipher(key, iv, AesCipherMode.CTR, pkcs7Padding: false)) },
+                    { "aes192-ctr", new CipherInfo(192, (key, iv) => new AesCipher(key, iv, AesCipherMode.CTR, pkcs7Padding: false)) },
+                    { "aes256-ctr", new CipherInfo(256, (key, iv) => new AesCipher(key, iv, AesCipherMode.CTR, pkcs7Padding: false)) },
+                    { "aes128-gcm@openssh.com", new CipherInfo(128, (key, iv) => new AesGcmCipher(key, iv), isAead: true) },
+                    { "aes256-gcm@openssh.com", new CipherInfo(256, (key, iv) => new AesGcmCipher(key, iv), isAead: true) },
+                    { "chacha20-poly1305@openssh.com", new CipherInfo(512, (key, iv) => new ChaCha20Poly1305Cipher(key), isAead: true) },
+                    { "aes128-cbc", new CipherInfo(128, (key, iv) => new AesCipher(key, iv, AesCipherMode.CBC, pkcs7Padding: false)) },
+                    { "aes192-cbc", new CipherInfo(192, (key, iv) => new AesCipher(key, iv, AesCipherMode.CBC, pkcs7Padding: false)) },
+                    { "aes256-cbc", new CipherInfo(256, (key, iv) => new AesCipher(key, iv, AesCipherMode.CBC, pkcs7Padding: false)) },
+                    { "3des-cbc", new CipherInfo(192, (key, iv) => new TripleDesCipher(key, new CbcCipherMode(iv), padding: null)) },
+                };
 
             HmacAlgorithms = new Dictionary<string, HashInfo>
                 {
---
+++ src/Renci.SshNet/Security/Cryptography/Ciphers/AesGcmCipher.BouncyCastleImpl.cs
@@ -0,0 +1,48 @@
+﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Modes;
+using Org.BouncyCastle.Crypto.Parameters;
+
+using Renci.SshNet.Common;
+using Renci.SshNet.Messages.Transport;
+
+namespace Renci.SshNet.Security.Cryptography.Ciphers
+{
+    internal partial class AesGcmCipher
+    {
+        private sealed class BouncyCastleImpl : Impl
+        {
+            private readonly GcmBlockCipher _cipher;
+            private readonly AeadParameters _parameters;
+
+            public BouncyCastleImpl(byte[] key, byte[] nonce)
+            {
+                _cipher = new GcmBlockCipher(new AesEngine());
+                _parameters = new AeadParameters(new KeyParameter(key), TagSizeInBytes * 8, nonce);
+            }
+
+            public override void Encrypt(byte[] input, int plainTextOffset, int plainTextLength, int associatedDataOffset, int associatedDataLength, byte[] output, int cipherTextOffset)
+            {
+                _cipher.Init(forEncryption: true, _parameters);
+                _cipher.ProcessAadBytes(input, associatedDataOffset, associatedDataLength);
+                var cipherTextLength = _cipher.ProcessBytes(input, plainTextOffset, plainTextLength, output, cipherTextOffset);
+                _ = _cipher.DoFinal(output, cipherTextOffset + cipherTextLength);
+            }
+
+            public override void Decrypt(byte[] input, int cipherTextOffset, int cipherTextLength, int associatedDataOffset, int associatedDataLength, byte[] output, int plainTextOffset)
+            {
+                _cipher.Init(forEncryption: false, _parameters);
+                _cipher.ProcessAadBytes(input, associatedDataOffset, associatedDataLength);
+                var plainTextLength = _cipher.ProcessBytes(input, cipherTextOffset, cipherTextLength + TagSizeInBytes, output, plainTextOffset);
+                try
+                {
+                    _ = _cipher.DoFinal(output, plainTextLength);
+                }
+                catch (InvalidCipherTextException ex)
+                {
+                    throw new SshConnectionException("MAC error", DisconnectReason.MacError, ex);
+                }
+            }
+        }
+    }
+}

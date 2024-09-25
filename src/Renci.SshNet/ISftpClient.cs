204636b44197d00ee87d8f3dae67b5f74a7a8162
Ryan Esteves <snargledorf@users.noreply.github.com>
2024-09-25 07:56:39 -0400

    Added DeleteAsync and DeleteDirectoryAsync to ISftpClient

--- src/Renci.SshNet/ISftpClient.cs
+++ src/Renci.SshNet/ISftpClient.cs
@@ -484,6 +484,14 @@ namespace Renci.SshNet
         void Delete(string path);
 
         /// <summary>
+        /// Permanently deletes a file on remote machine.
+        /// </summary>
+        /// <param name="path">The name of the file or directory to be deleted. Wildcard characters are not supported.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> to observe.</param>
+        /// <returns>A <see cref="Task"/> that represents the asynchronous delete operation.</returns>
+        Task DeleteAsync(string path, CancellationToken cancellationToken = default);
+
+        /// <summary>
         /// Deletes remote directory specified by path.
         /// </summary>
         /// <param name="path">Directory to be deleted path.</param>
@@ -494,6 +502,20 @@ namespace Renci.SshNet
         /// <exception cref="SshException">A SSH error where <see cref="Exception.Message"/> is the message from the remote host.</exception>
         /// <exception cref="ObjectDisposedException">The method was called after the client was disposed.</exception>
         void DeleteDirectory(string path);
+
+        /// <summary>
+        /// Asynchronously deletes a remote directory.
+        /// </summary>
+        /// <param name="path">The path of the directory to be deleted.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> to observe.</param>
+        /// <returns>A <see cref="Task"/> that represents the asynchronous delete operation.</returns>
+        /// <exception cref="ArgumentException"><paramref name="path"/> is <see langword="null"/> or contains only whitespace characters.</exception>
+        /// <exception cref="SshConnectionException">Client is not connected.</exception>
+        /// <exception cref="SftpPathNotFoundException"><paramref name="path"/> was not found on the remote host.</exception>
+        /// <exception cref="SftpPermissionDeniedException">Permission to delete the directory was denied by the remote host. <para>-or-</para> A SSH command was denied by the server.</exception>
+        /// <exception cref="SshException">A SSH error where <see cref="Exception.Message"/> is the message from the remote host.</exception>
+        /// <exception cref="ObjectDisposedException">The method was called after the client was disposed.</exception>
+        Task DeleteDirectoryAsync(string path, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Deletes remote file specified by path.

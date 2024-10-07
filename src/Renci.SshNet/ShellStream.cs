5a1640b0c0815b48533371841429b0775cd95347
oheiheiheiheihei <294797392@qq.com>
2024-10-07 15:27:49 +0800

    Improve ShellStream
    * Added SendWindowChangeRequest to ShellStream
    
    * Added a new constructor to ShellStream
    
    * SshClient adds an overload function for CreateShellStream
    
    When creating ShellStream, the channel will be immediately opened in the constructor of ShellStream. At this time, the calling module may not have subscribed to ShellStream's DataReceived event, which may result in the loss of a small piece of data immediately after connection.
    You can use ShellStream's new constructor to solve this problem

--- src/Renci.SshNet/ShellStream.cs
+++ src/Renci.SshNet/ShellStream.cs
@@ -94,16 +94,16 @@ namespace Renci.SshNet
         /// <param name="height">The terminal height in pixels.</param>
         /// <param name="terminalModeValues">The terminal mode values.</param>
         /// <param name="bufferSize">The size of the buffer.</param>
-        /// <param name="dataReceived">The DataReceived Handler.</param>
-        /// <param name="errorOccurred">The ErrorOccurred Handler.</param>
+        /// <param name="dataReceived">The DataReceived Handler.Please note that this event needs to be manually unsubscribed.</param>
+        /// <param name="errorOccurred">The ErrorOccurred Handler.Please note that this event needs to be manually unsubscribed.</param>
         /// <exception cref="SshException">The channel could not be opened.</exception>
         /// <exception cref="SshException">The pseudo-terminal request was not accepted by the server.</exception>
         /// <exception cref="SshException">The request to start a shell was not accepted by the server.</exception>
         internal ShellStream(ISession session, string terminalName, uint columns, uint rows, uint width, uint height, IDictionary<TerminalModes, uint> terminalModeValues, int bufferSize, EventHandler<ShellDataEventArgs> dataReceived, EventHandler<ExceptionEventArgs> errorOccurred)
                : this(session, terminalName, columns, rows, width, height, terminalModeValues, bufferSize)
         {
-            DataReceived = dataReceived;
-            ErrorOccurred = errorOccurred;
+            DataReceived += dataReceived;
+            ErrorOccurred += errorOccurred;
         }
 
         /// <summary>

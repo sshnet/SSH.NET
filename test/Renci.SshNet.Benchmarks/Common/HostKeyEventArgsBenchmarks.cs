e059332bb2150c0dbf9542e637ee30b91b37717b
Marius Thesing <marius.thesing@gmail.com>
2025-02-02 19:51:36 +0100

    fix null reference warnings in Benchmarks
    seems like the warnings were (somehow) disabled here
    before and were fixed by the previous TargetFrameworkIdentifier
    change.

--- test/Renci.SshNet.Benchmarks/Common/HostKeyEventArgsBenchmarks.cs
+++ test/Renci.SshNet.Benchmarks/Common/HostKeyEventArgsBenchmarks.cs
@@ -19,7 +19,7 @@ namespace Renci.SshNet.Benchmarks.Common
         {
             using (var s = typeof(HostKeyEventArgsBenchmarks).Assembly.GetManifestResourceStream("Renci.SshNet.Benchmarks.Data.Key.RSA.txt"))
             {
-                var privateKey = new PrivateKeyFile(s);
+                var privateKey = new PrivateKeyFile(s!);
                 return (KeyHostAlgorithm)privateKey.HostKeyAlgorithms.First();
             }
         }

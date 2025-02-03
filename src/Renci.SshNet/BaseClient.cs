e74a3bd674fd9dc34bda9647a6a5e7039b750e95
Vyacheslav Brevnov <softstonedevelop@gmail.com>
2025-02-03 10:26:35 +0300

    fix: UnhandledException: System.ObjectDisposedException. Error on thread pool
    #1588

--- src/Renci.SshNet/BaseClient.cs
+++ src/Renci.SshNet/BaseClient.cs
@@ -502,6 +502,14 @@ namespace Renci.SshNet
                 {
                     _ = session.TrySendMessage(new IgnoreMessage());
                 }
+                catch (ObjectDisposedException)
+                {
+                    // ignore
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error in TrySendMessage");
+                }
                 finally
                 {
                     Monitor.Exit(_keepAliveLock);

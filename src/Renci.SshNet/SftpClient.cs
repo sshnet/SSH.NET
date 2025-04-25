f5075857848a4b50ceb7798350cfce13f0d46fb0
Noah Dela Rosa <ndelarosa@stratadecision.com>
2025-04-25 10:15:26 -0500

    dispose already closes, remove dup call

--- src/Renci.SshNet/SftpClient.cs
+++ src/Renci.SshNet/SftpClient.cs
@@ -2518,20 +2518,12 @@ namespace Renci.SshNet
             }
 
             var fullPath = await _sftpSession.GetCanonicalPathAsync(path, cancellationToken).ConfigureAwait(false);
-
             var handle = await _sftpSession.RequestOpenAsync(fullPath, Flags.Read, cancellationToken).ConfigureAwait(false);
-            try
-            {
-                using (var input = new SftpFileStream(_sftpSession, fullPath, FileAccess.Read, (int)_bufferSize, handle, 0L))
-                {
-                    var bufferSize = (int)_sftpSession.CalculateOptimalReadLength(_bufferSize);
 
-                    await input.CopyToAsync(output, bufferSize, cancellationToken).ConfigureAwait(false);
-                }
-            }
-            finally
+            using (var input = new SftpFileStream(_sftpSession, fullPath, FileAccess.Read, (int)_bufferSize, handle, 0L))
             {
-                await _sftpSession.RequestCloseAsync(handle, cancellationToken).ConfigureAwait(false);
+                var bufferSize = (int)_sftpSession.CalculateOptimalReadLength(_bufferSize);
+                await input.CopyToAsync(output, bufferSize, cancellationToken).ConfigureAwait(false);
             }
         }
 
@@ -2628,13 +2620,11 @@ namespace Renci.SshNet
             }
 
             var fullPath = await _sftpSession.GetCanonicalPathAsync(path, cancellationToken).ConfigureAwait(false);
-
             var handle = await _sftpSession.RequestOpenAsync(fullPath, Flags.Write | flags, cancellationToken).ConfigureAwait(false);
 
             using (var output = new SftpFileStream(_sftpSession, fullPath, FileAccess.Write, (int)_bufferSize, handle, 0L))
             {
                 var bufferSize = (int)_sftpSession.CalculateOptimalWriteLength(_bufferSize, handle);
-
                 await input.CopyToAsync(output, bufferSize, cancellationToken).ConfigureAwait(false);
             }
         }

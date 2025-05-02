d5b6333e0292ff581662e59da4623b9eab1f2fda
Noah Dela Rosa <ndelarosa@stratadecision.com>
2025-05-02 10:47:56 -0500

    missed one configure await

--- test/Renci.SshNet.IntegrationTests/SftpTests.cs
+++ test/Renci.SshNet.IntegrationTests/SftpTests.cs
@@ -4115,7 +4115,7 @@ namespace Renci.SshNet.IntegrationTests
                     {
                         uploadStream.Position = 0;
 
-                        await client.UploadFileAsync(uploadStream, "gert.txt");
+                        await client.UploadFileAsync(uploadStream, "gert.txt").ConfigureAwait(false);
 
                         uploadStream.Position = 0;
 

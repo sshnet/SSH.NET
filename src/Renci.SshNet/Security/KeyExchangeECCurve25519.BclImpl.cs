05ff68968e486f0dd7d59887f7ad1f93a93e9741
Scott Xu <scott-xu@msn.com>
2025-09-28 07:48:08 +0800

    Create dedicated KeyExchangeECCurve25519 BclImpl

--- src/Renci.SshNet/Security/KeyExchangeECCurve25519.BclImpl.cs
+++ src/Renci.SshNet/Security/KeyExchangeECCurve25519.BclImpl.cs
@@ -1,10 +1,9 @@
 ﻿#if NET
-using System;
 using System.Security.Cryptography;
 
 namespace Renci.SshNet.Security
 {
-    internal abstract partial class KeyExchangeEC
+    internal abstract partial class KeyExchangeECCurve25519
     {
         protected sealed class BclImpl : Impl
         {
@@ -23,43 +22,24 @@ namespace Renci.SshNet.Security
 
                 var q = _clientECDH.PublicKey.ExportParameters().Q;
 
-                return EncodeECPoint(q);
+                return q.X;
             }
 
             public override byte[] CalculateAgreement(byte[] serverECPoint)
             {
-                var q = DecodeECPoint(serverECPoint);
-
                 var parameters = new ECParameters
                 {
                     Curve = _curve,
-                    Q = q,
+                    Q = new ECPoint
+                    {
+                        X = serverECPoint,
+                        Y = new byte[serverECPoint.Length]
+                    },
                 };
 
                 using var serverECDH = ECDiffieHellman.Create(parameters);
 
                 return _clientECDH.DeriveRawSecretAgreement(serverECDH.PublicKey);
-            }
-
-            private static byte[] EncodeECPoint(ECPoint point)
-            {
-                var q = new byte[1 + point.X.Length + point.Y.Length];
-                q[0] = 0x04;
-                Buffer.BlockCopy(point.X, 0, q, 1, point.X.Length);
-                Buffer.BlockCopy(point.Y, 0, q, point.X.Length + 1, point.Y.Length);
-
-                return q;
-            }
-
-            private static ECPoint DecodeECPoint(byte[] q)
-            {
-                var cordSize = (q.Length - 1) / 2;
-                var x = new byte[cordSize];
-                var y = new byte[cordSize];
-                Buffer.BlockCopy(q, 1, x, 0, x.Length);
-                Buffer.BlockCopy(q, cordSize + 1, y, 0, y.Length);
-
-                return new ECPoint { X = x, Y = y };
             }
 
             protected override void Dispose(bool disposing)

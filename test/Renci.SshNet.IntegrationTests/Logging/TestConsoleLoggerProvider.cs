9f1933354b27dc3a84e79b1309e8432a5f3561e5
Marius Thesing <marius.thesing@gmail.com>
2025-11-15 09:55:28 +0100

    disable preview analyzers
    reverts 6c3c06d95a2695bf2fa582156a01431843bee46b

--- test/Renci.SshNet.IntegrationTests/Logging/TestConsoleLoggerProvider.cs
+++ test/Renci.SshNet.IntegrationTests/Logging/TestConsoleLoggerProvider.cs
@@ -4,9 +4,7 @@ using Microsoft.Extensions.Logging;
 
 namespace Renci.SshNet.IntegrationTests.Logging
 {
-#pragma warning disable CA1812 // Avoid uninstantiated internal classes
     internal class TestConsoleLoggerProvider : ILoggerProvider
-#pragma warning restore CA1812 // Avoid uninstantiated internal classes
     {
         public ILogger CreateLogger(string categoryName)
         {
